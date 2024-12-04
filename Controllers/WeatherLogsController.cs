8a26aec10831c6781fababb91c53e0f45c47f011
Bogdan Ciora <bogdan.ciora@qubiz.com>
2024-12-04 15:36:34 +0200

    Added weather logs page structure

---
+++ Controllers/WeatherLogsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class WeatherLogsController : Controller
+{
+    private readonly WeatherLogsService _weatherLogsService;
+
+    public WeatherLogsController(WeatherLogsService weatherLogsService)
+    {
+        _weatherLogsService = weatherLogsService;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> GetWeatherLogs()
+    {
+        var weatherLogs = await _weatherLogsService.GetWeatherLogsAsync();
+        return View("Index", weatherLogs);
+    }
+}
---
+++ Services/WeatherLogsService.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json;
+
+public class WeatherLogsService
+{
+    private readonly HttpClient _httpClient;
+    private readonly string _apiKey;
+    private const string BaseUrl = "https://api.openweathermap.org/data/2.5/weather";
+
+    public WeatherLogsService(HttpClient httpClient, string apiKey)
+    {
+        _httpClient = httpClient;
+        _apiKey = apiKey;
+    }
+
+    public async Task<WeatherLogData> GetWeatherLogsAsync()
+    {
+        return new WeatherLogData();
+    }
+}
+
+public class WeatherLogData
+{
+    public string DateTime { get; set; }
+    
+    public string CityName { get; set; }
+
+    public float Temparature { get; set; }
+
+    public float Humidity { get; set; }
+}
